0fc99b254c0121d9519e6dac61671f60e738d0a4
Slavi <slavicvetanov@gmail.com>
2015-03-10 13:10:22 +0200

    Update to CountriesCtrl

--- www/js/controllers/CountriesCtrl.js
+++ www/js/controllers/CountriesCtrl.js
@@ -1,10 +1,15 @@
 /**
  * Created by Slavi on 3/7/2015.
  */
-app.controller('CountriesCtrl', ['$scope', function($scope){
+app.controller('CountriesCtrl', function($scope, cashedResourcesService){
     'use strict';
 
+    $scope.isSearchActive = false;
+    $scope.search = {};
+    $scope.countries = cashedResourcesService.getAll();
+    $scope.toggleSearch = function(){
+        $scope.search.name = '';
+        $scope.isSearchActive = !$scope.isSearchActive;
+    }
 
-
-
-}]);
+});
